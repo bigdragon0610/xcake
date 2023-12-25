21f42b35059b79d49617d1844080d05e38cb2712
ryudai <bigdragon.06102001@gmail.com>
2023-12-26 00:24:07 +0900

    複数行の入力に対応

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io::{self, Read};
+
 fn main() {
     let emojis = "🍰🎂🎉🎅🛷🎄🍗🎁🍾🔔".chars().collect::<Vec<char>>();
-    let i = fastrand::usize(..emojis.len());
-    println!("{}", emojis[i]);
+    let ends = ['。', '!', '?', '！', '？'];
+
+    let create_emoji = || {
+        let i = fastrand::usize(..emojis.len());
+        emojis[i]
+    };
+
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    input = input
+        .chars()
+        .map(|c| if ends.contains(&c) { create_emoji() } else { c })
+        .collect::<String>();
+
+    println!("{}", input);
 }
